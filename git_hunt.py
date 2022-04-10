4d6f6d067236a5a98d7241d8ea4a92512ad47ea3
Blazej Michalik <blazej.michalik@nokia.com>
2022-04-10 03:54:00 +0200

    Base setup: git-hunt command

---
+++ git_hunt.py
@@ -0,0 +1,17 @@
+#!/bin/env python
+import sys
+import subprocess
+from pathlib import Path
+
+
+def git_blame(path: Path):
+    return subprocess.check_output(["git", "blame", str(path)])
+
+
+def __main__():
+    path = Path(sys.argv[1])
+    blame = git_blame(path)
+    print(blame)
+
+
+__main__()
