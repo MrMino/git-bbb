8a4f356f6c433084e1c39f46d19a4f859125bc96
Blazej Michalik <blazej.michalik@nokia.com>
2022-04-10 21:51:46 +0200

    BlameLine dataclass

--- git_hunt.py
+++ git_hunt.py
@@ -2,6 +2,31 @@
 import sys
 import subprocess
 from pathlib import Path
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class BlameLine:
+    content: str
+
+    sha: str
+    previous_sha: Optional[str]
+    summary: str
+
+    original_filename: str
+    original_line_number: int
+    final_line_number: int
+
+    author_name: str
+    author_email: str
+    author_time: int
+    author_tz: str
+
+    committer_name: str
+    committer_email: str
+    committer_time: int
+    committer_tz: str
 
 
 def git_blame(path: Path):
