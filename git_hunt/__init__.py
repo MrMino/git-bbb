0f44c2cf787eac48dfd3b5018857b4085dc649b9
Blazej Michalik <blazej.michalik@nokia.com>
2022-05-08 22:20:09 +0200

    Fix IndexError when viewing smaller files

--- git_hunt/__init__.py
+++ git_hunt/__init__.py
@@ -31,14 +31,16 @@ MAX_SHA_CHARS_SHOWN = 16
 class CommitSHAMargin(Margin):
     def __init__(self, shas: List[str]):
         self.shas = shas
+        self.max_height = len(shas)
 
     def create_margin(
         self, winfo: WindowRenderInfo, width: int, height: int
     ) -> StyleAndTextTuples:
         start = winfo.vertical_scroll
+        end = min(start + self.max_height, start + height)
         # TODO: mouse click on the margin should change cursor position
         margin_text: StyleAndTextTuples = [
-            ("", self.shas[n] + "\n") for n in range(start, start + height)
+            ("", self.shas[n] + "\n") for n in range(start, end)
         ]
         self._highlight_current_line(winfo, margin_text)
 
