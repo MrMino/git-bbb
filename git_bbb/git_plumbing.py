2707049acfd7119eddba7c18edca7d97b941f3f3
Blazej Michalik <blazej.michalik@nokia.com>
2022-05-25 19:01:02 +0200

    Add a TODO

--- git_bbb/git_plumbing.py
+++ git_bbb/git_plumbing.py
@@ -156,6 +156,9 @@ DEFAULT_IGNORE_REVS_PATH = Path(".git-ignore-revs")
 
 def default_ignore_revs() -> Optional[str]:
     """Check if default ignore-revs file is available, return its path if so."""
+    # TODO: either use git_show_toplevel here and remove dependency on
+    # gitpython, or reuse repo.working_tree_dir where git_show_toplevel is
+    # used.
     repo = git.Repo(search_parent_directories=True)
     worktree_path = repo.working_tree_dir
 
